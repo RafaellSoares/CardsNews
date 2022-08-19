4483875e4ab0c4a6dbe48c99c6ad05e6efcad3c1
RafaellSoares <rafaelsoares199911@gmail.com>
2022-08-19 14:12:07 -0300

    Initial commit

---
+++ src/components/CardNews.jsx
@@ -0,0 +1,17 @@
+import { useState, Component } from 'react';
+
+export default function CardNews() {
+  return (
+    <div>
+      <div className="card">
+        <div className="foto">
+          <img src="/vite.svg" alt="" />
+        </div>
+        <div className="conteudo">
+          <h2>Titulo</h2>
+          <p>dsadda</p>
+        </div>
+      </div>
+    </div>
+  );
+}
